75e62d5ca6d6634242e9fc47781db99e897f2d32
Larry E <le91688@gmail.com>
2018-11-14 19:36:35 +0000

    Created the find_offset.sh bash script

---
+++ find_offset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author: Xdude736
+#Disclaimer: This is my variation of the offset test found on page 381 in Hacking, the Art of Exploitation 2nd by Jon Erikson
+#Usage: ./find_offset <prog_test_path> <number_of_tests>
+# prog_test_path = the file path of the program to be tested
+# number_of_tests = upper limit of the loop
+
+if $# != 2 || $# != 3; then
+    echo "Usage: ./find_offset <prog_test_path> <number_of_tests>"
+    break
+fi
