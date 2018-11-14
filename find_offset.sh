0f73ef4bac14cc37e32b3820c3ccc4638fddbc88
Xdude736 <38534944+Xdude736@users.noreply.github.com>
2018-11-14 14:38:30 -0500

    Update find_offset.sh

--- find_offset.sh
+++ find_offset.sh
@@ -5,7 +5,7 @@
 # prog_test_path = the file path of the program to be tested
 # number_of_tests = upper limit of the loop
 
-if $# != 2 || $# != 3; then
+if [ "$#" != 2 || "$#" != 3 ]; then
     echo "Usage: ./find_offset <prog_test_path> <number_of_tests>"
     break
 fi
